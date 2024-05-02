f0931dfc1eb3513cb1c3a25e1e62ebf58282895f
asdwerte1 <samjmarkham@outlook.com>
2024-05-02 15:12:59 +0100

    Solution

--- DaysOfTheWeek.py
+++ DaysOfTheWeek.py
@@ -3,30 +3,28 @@ class WeekDayError(Exception):
 	
 
 class Weeker:
-    #
-    # Write code here.
-    #
 
     def __init__(self, day):
-        #
-        # Write code here.
-        #
+        self.__days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        if day not in self.__days: raise WeekDayError
+        else: self.__day = day
 
     def __str__(self):
-        #
-        # Write code here.
-        #
+        return self.__day
 
     def add_days(self, n):
-        #
-        # Write code here.
-        #
+        day_diff = n % 7 # Number of days to add
+        current_day = self.__days.index(self.__day)
+        new_day = day_diff + current_day
+        if new_day >= 7: new_day -= 7 # Remove overflow - sets index of day of week
+        self.__day = self.__days[new_day]
 
     def subtract_days(self, n):
-        #
-        # Write code here.
-        #
-
+        day_diff = n % 7 # Number of days to remove
+        current_day = self.__days.index(self.__day)
+        new_day = current_day - day_diff
+        if new_day < 0: new_day = 7 - abs(new_day) # Remove overflow - sets index of day of week
+        self.__day = self.__days[new_day]
 
 try:
     weekday = Weeker('Mon')
